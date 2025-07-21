52d80369509837db7e816059cc11d4088464ffbb
Adeel Aj <146657889+Adeel-Ul-Rehman@users.noreply.github.com>
2025-07-21 09:21:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://riderexpense.free.nf",
+    "http://localhost:3000",
   "https://rider-expense.onrender.com",
   "https://rider-expense-backend.onrender.com"
 ];
